c09912c234614df4a7051c40a863f482f8bfbb79
DineshDDi <70742612+DineshDDi@users.noreply.github.com>
2023-08-30 17:16:23 +0530

    Update views.py

--- testapp/views.py
+++ testapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<html><h1>This is EKS test page-1</h1></html>")
+    return HttpResponse("<html><h1>This is EKS test page</h1></html>")
 
 
 
