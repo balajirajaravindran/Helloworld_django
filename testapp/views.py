1a664a3636218b286eba24b379d4d1e739d6f010
balajirajaravindran <103511691+balajirajaravindran@users.noreply.github.com>
2024-01-31 17:22:29 +0530

    Update views.py

--- testapp/views.py
+++ testapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<html><h1>This is EKS test page version-1.0</h1></html>")
+    return HttpResponse("<html><h1>This is EKS test page version-2.0</h1></html>")
 
 
 
